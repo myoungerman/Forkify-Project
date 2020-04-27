e31128be6d76c6c260beb6ba4af93fb973ce2c47
Mitchell Youngerman <MYounge@am.trimblecorp.net>
2020-04-26 22:15:20 -0400

    Added event handlers and a function to toggle the sidebar buttons
    Updates the page buttons as the user moves to the next or the previous page.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,21 +1,26 @@
 export { displaySearchResults };
+export { previousBtn };
+export { nextBtn };
+export { switchPage };
 
-let nextPageBtn = document.querySelector('.results__pages');
-nextPageBtn.style.display = "none";
+let currResultsPage = 0;
+let previousBtn = document.querySelector('.results__btn--prev');
+let nextBtn = document.querySelector('.results__btn--next');
+let numDivs = 0;
+
+previousBtn.style.display = 'none';
+nextBtn.style.display = 'none';
 
 function displaySearchResults (data) {
     let count = 0;
     const numRecipeResults = data.recipes.length;
     let numOfListItems = 0;
 
-    document.querySelector('.results__btn--next').style.display = "block";
-    nextPageBtn.querySelector('.results__btn--next').querySelector('span').textContent = 'Page 2';
-
     for (count = 0; count < numRecipeResults; count++) {
         let currRecipe = data.recipes[count];
         let currListItem = 0;
 
-        document.querySelector('.resultsPageNumber').insertAdjacentHTML('beforeend', '<li><a class="results__link results__link--active" href="123"><figure class="results__fig"><img src="" alt="Test"></figure><div class="results__data"><h4 class="results__name">Recipe Name</h4><p class="results__author">Recipe Author</p></div></a></li>');
+        document.querySelector('.resultsPageNumber').insertAdjacentHTML('beforeend', '<li><a class="results__link" href="123"><figure class="results__fig"><img src="" alt="Test"></figure><div class="results__data"><h4 class="results__name">Recipe Name</h4><p class="results__author">Recipe Author</p></div></a></li>');
         numOfListItems = document.querySelector('.resultsPageNumber').querySelectorAll('li');
         currListItem = numOfListItems[count]; 
 
@@ -37,6 +42,43 @@ function displaySearchResults (data) {
         document.querySelector(`.page${parentDiv}`).append(numOfListItems[j]);
         if ((j + 1) % 10 === 0) {
             parentDiv += 1;
+        }
+    }
+
+    numDivs = document.querySelectorAll('[class^="page"]');
+
+    for (let k = 1; k < numDivs.length; k++) {
+        numDivs[k].style.display = 'none';
+    }
+
+    nextBtn.style.display = 'block';
+    nextBtn.querySelector('span').textContent = 'Page 2';
+}
+
+function switchPage(pageToLoad) {
+     if (pageToLoad === 'next') { // Load the next page.
+        document.querySelector(`.page${currResultsPage}`).style.display = 'none';
+        currResultsPage += 1;
+        document.querySelector(`.page${currResultsPage}`).style.display = 'block';
+        previousBtn.style.display = 'block';
+        previousBtn.querySelector('span').textContent = `Page ${currResultsPage}`;
+        nextBtn.querySelector('span').textContent = `Page ${currResultsPage + 2}`;
+
+        if (currResultsPage == (numDivs.length - 1)) { // Hide the next button if this is the last page.
+            nextBtn.style.display = 'none';
+        } 
+    } else { // Load the previous page.
+        document.querySelector(`.page${currResultsPage}`).style.display = 'none';
+        currResultsPage -= 1;
+        document.querySelector(`.page${currResultsPage}`).style.display = 'block';
+        previousBtn.querySelector('span').textContent = `Page ${currResultsPage}`;
+        nextBtn.querySelector('span').textContent = `Page ${currResultsPage + 2}`;
+
+        if (currResultsPage == (numDivs.length - 2)) { // Show the next button if another page exists.
+            nextBtn.style.display = 'block';
+        } 
+        if (currResultsPage == 0) { // Hide the previous button if this is the first page.
+            previousBtn.style.display = 'none';
         }
     }
 }
