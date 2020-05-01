ac2025f4951e4b1fbcc31e79dedda2dd23ba5de0
Mitchell Youngerman <MYounge@am.trimblecorp.net>
2020-05-01 16:37:02 -0400

    Added Shopping List click event

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,10 @@
 import { searchForRecipeAW } from './models/Search';
 import { displaySearchResults } from './views/searchView';
-import { previousBtn } from './views/searchView';
-import { nextBtn } from './views/searchView';
 import { switchPage } from './views/searchView';
 import { loadRecipeAW } from './models/Recipe';
 import { displayRecipeDetails } from './views/recipeView';
 
+// Clicking Search
 document.querySelector('.search__btn').addEventListener('click', (event) => {
     event.preventDefault();
     searchForRecipeAW().then(data => {
@@ -13,11 +12,13 @@ document.querySelector('.search__btn').addEventListener('click', (event) => {
     });
 });
 
+// Clicking to move forward or backward a page of recipe search results
 document.querySelector('.results__pages').addEventListener('click', () => {
     if (event.target.className == 'btn-inline results__btn--next') { switchPage('next'); }
     if (event.target.className == 'btn-inline results__btn--prev') { switchPage('prev'); }
 });
 
+// Clicking a recipe in the search results
 document.querySelector('.resultsPageNumber').addEventListener('click', (event) => {
     let listItems = document.querySelectorAll('li');
     let recipeID = '';
@@ -28,8 +29,14 @@ document.querySelector('.resultsPageNumber').addEventListener('click', (event) =
             recipeID = listItems[i].querySelector('.results__link').getAttribute('href');
         }
     }
-    
+
     loadRecipeAW(recipeID).then(recipeObj => {
         displayRecipeDetails(recipeObj);
     });
+});
+
+// Clicking Add to Shopping List
+document.querySelector('body').addEventListener('click', (event) => {
+    let addtoListBtn = document.querySelector('.btn-small, recipe__btn');
+    if (addtoListBtn.contains(event.target)) {  } // get the current li of the recipe, then pass it to List
 });
